3f51fdc12f3dacbd2470e9155cd42676b8310f4d
ak3in2d <abhilashkar2007@gmail.com>
2025-11-16 12:28:46 +0530

    Reboot. I can't handle my spaghetti code anymore.

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import ClickSpark from "./ClickSpark.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <ClickSpark sparkColor="#ffd166" sparkCount={14}>
+            <App />
+        </ClickSpark>
+    </React.StrictMode>
+);
